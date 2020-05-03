19f17c4cf4eb6a84a114504a5e15d6ea3af7d843
andrea96 <andrea.ciceri@autistici.org>
2020-05-03 16:07:44 +0200

    Playing with travis

--- default.nix
+++ default.nix
@@ -27,7 +27,7 @@ let
   blog-deploy = pkgs.writeShellScriptBin "blog-deploy"
     ''
       echo "test" > test.txt
-      ${pkgs.rsync}/bin/rsync -avz -e "ssh -i /tmp/deploy_rsa" test.txt andrea@cc0.tech:~/www/ 
+      ${pkgs.rsync}/bin/rsync -avz -e "${pkgs.openssh}/bin/ssh -i /tmp/deploy_rsa" test.txt andrea@cc0.tech:~/www/ 
     '';
 in
 stdenv.mkDerivation rec {
