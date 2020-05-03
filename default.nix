18f8b5e8b86d5b346c34ac7283549ba07dda7add
andrea96 <andrea.ciceri@autistici.org>
2020-05-03 15:49:00 +0200

    Playing with travis

--- default.nix
+++ default.nix
@@ -26,10 +26,6 @@ let
     '';
   blog-deploy = pkgs.writeShellScriptBin "blog-deploy"
     ''
-      ${pkgs.openssl}/bin/openssl aes-256-cbc -K $encrypted_db2095f63ba3_key -iv $encrypted_db2095f63ba3_iv -in deploy_rsa.enc -out /tmp/deploy_rsa -d
-      eval "$(ssh-agent -s)"
-      chmod 600 /tmp/deploy_rsa
-      ssh-add /tmp/deploy_rsa
       echo "test" > test.txt
       ${pkgs.openssh}/bin/scp test.txt andrea@cc0.tech:~/www/ 
     '';
